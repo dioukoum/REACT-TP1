a02ea6832d5b63b08ede1b5489387c371cb0226b
Sissoko Dioukou Moussa <dioukoum125@gmail.com>
2023-03-03 03:29:58 +0000

    tp1-react-gl3

--- coversion/src/App.js
+++ coversion/src/App.js
@@ -1,4 +1,7 @@
 import React, { useState } from "react";
+import "./App.css";
+import boilingWater from "./assets/boilingWater.jpg";
+import noBoilingWater from "./assets/noBoilingWater.jpg";
 
 export default function App() {
   const [degC, setDegC] = useState(0);
@@ -9,7 +12,20 @@ export default function App() {
     return (degC * 9) / 5 + 32;
   }
   function isBout(degC) {
-    return degC >= 100 ? "L'eau bout" : "L'eau nebout pas";
+    return degC >= 100 ? "L'eau bout" : "L'eau ne bout pas";
+  }
+  function imgs() {
+    if (degC >= 100) {
+      return boilingWater;
+    } else {
+      return noBoilingWater;
+    }
+  }
+  const divColor = {};
+  if (degC < 100) {
+    divColor.color = "skyblue";
+  } else {
+    divColor.color = "red";
   }
   console.log(degC);
   return (
@@ -17,15 +33,21 @@ export default function App() {
       <label>
         Temperature (en Celsus)
         <br />
-        <input type="text" onChange={handleChange} value={degC} />
+        <input
+          type="text"
+          onChange={handleChange}
+          value={degC}
+          style={divColor}
+        />
       </label>
       <br />
       <label>
-        Temperature (en Fahrenhei)
+        Temperature (en Fahrenheit)
         <br />
-        <input type="text" value={convertir(degC)} />
+        <input type="text" value={convertir(degC)} style={divColor} />
       </label>
       <p>{isBout(degC)}</p>
+      <img src={imgs()} alt="bout" />
     </div>
   );
 }
